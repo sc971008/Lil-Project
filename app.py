4460c9215c5a52f8580d7a8685eb9d0de9bd5c61
ChengShi <sc971008@gmail.com>
2025-07-06 01:26:01 -0400

    aba

--- app.py
+++ app.py
@@ -106,5 +106,8 @@ def index():
             return generate_excel(all_records)
     return render_template("index.html")
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
